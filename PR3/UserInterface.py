c1280a003ba0094c2d9d4fee2c4c5726f6916c49
Dimon-Pokemon <purumamuma@gmail.com>
2023-11-25 21:19:48 +0400

    Графический интерфейс главного окна приложения - чата

--- PR3/UserInterface.py
+++ PR3/UserInterface.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import *
 from tkinter import ttk
 from Client import Client
@@ -36,7 +37,7 @@ class UserInterface:
             input_name.get(),
             input_server_host.get(),
             int(input_server_port.get())
-        ), print("Клиент создан. Нужно удалить окно!"), self.connectToChatWindow.destroy()))
+        ), self.connectToChatWindow.destroy(), self.start_main_window()))
         separator = ttk.Separator(self.connectToChatWindow)
         create = Button(self.connectToChatWindow, text="Создать чат")
 
@@ -56,6 +57,36 @@ class UserInterface:
         create.pack()
 
         self.connectToChatWindow.mainloop()
+
+    def start_main_window(self):
+        x = 400
+        y = 600
+        self.mainWindow = Tk()
+        self.mainWindow.geometry(f"{x}x{y}")
+
+        list_box_connections = Listbox(self.mainWindow)
+        for i in self.main.client.connections.values():
+            list_box_connections.insert(0, i)
+
+        messages_frame = ttk.Frame(self.mainWindow, borderwidth=1, relief=SOLID, padding=[8, 10])
+        scroll_bar = tkinter.Scrollbar(messages_frame)
+
+        entry_message = Entry(self.mainWindow)
+
+        button_send_message = Button(self.mainWindow, text="Отправить")
+
+        list_box_connections_WIDTH = 75
+        messages_frame_height = y - button_send_message.winfo_reqheight()
+        entry_message_WIDTH = 225
+
+        list_box_connections.place(x=0, y=0, width=list_box_connections_WIDTH, height=600)
+        messages_frame.place(x=list_box_connections_WIDTH, y=0, width=325, height=messages_frame_height)
+        scroll_bar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+        entry_message.place(x=list_box_connections_WIDTH, y=messages_frame_height, width=entry_message_WIDTH)
+        button_send_message.place(x=entry_message_WIDTH+list_box_connections_WIDTH, y=messages_frame_height, width=100)
+
+        self.mainWindow.mainloop()
+
 
 
 
